edcdea1c8f98cf61621476a038b232828a13a10e
dhyunkim <imzan@naver.com>
2023-12-01 23:02:59 +0900

    typeorm 및 redis 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { SellerModule } from './seller/seller.module';
 import { CustomerModule } from './customer/customer.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 
 @Module({
   imports: [
@@ -14,6 +16,13 @@ import { CustomerModule } from './customer/customer.module';
       playground: process.env.NODE_ENV !== 'production',
       autoSchemaFile: true,
       context: ({ req, res }) => ({ req, res }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/migration/*.{ts,js}'],
+        }),
     }),
     SellerModule,
     CustomerModule,
