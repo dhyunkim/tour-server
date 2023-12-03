7224258d473348eb75fa98f4a276020dd9d47040
dhyunkim <imzan@naver.com>
2023-12-03 22:40:42 +0900

    validation 추가

--- src/tour-holiday/tour-holiday.service.ts
+++ src/tour-holiday/tour-holiday.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { TourHolidayRepository } from './repository';
@@ -25,11 +25,27 @@ export class TourHolidayService {
   }
 
   async addTourWeekHoliday(args: IAddTourWeekHoliday) {
+    const weekHoliday = await this.tourHolidayRepository.getOneByWeek(
+      args.tourId,
+      args.week,
+    );
+    if (weekHoliday) {
+      throw new BadRequestException('이미 등록된 휴일입니다.');
+    }
+
     const addResult = await this.tourHolidayRepository.addWeekHoliday(args);
     return addResult.id;
   }
 
   async addTourSpecificHoliday(args: IAddTourSpecificHoliday) {
+    const specificHoliday = await this.tourHolidayRepository.getOneBySpecific(
+      args.tourId,
+      args.specific,
+    );
+    if (specificHoliday) {
+      throw new BadRequestException('이미 등록된 휴일입니다.');
+    }
+
     const addResult = await this.tourHolidayRepository.addSpecificHoliday(args);
     return addResult.id;
   }
