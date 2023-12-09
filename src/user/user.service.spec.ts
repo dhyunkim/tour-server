3bf9b3dcdfa12cc39ed9027091b08bc9f5ae2467
dhyunkim <imzan@naver.com>
2023-12-10 02:11:12 +0900

    user service에 테스트코드 추가

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,77 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { UserRepository } from './repository';
+import { MockUserRepository } from '../../test/repository';
+import { User } from './entity';
+import { IAddUser } from './interface';
 
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: UserRepository,
+          useValue: MockUserRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    userRepository = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getUserByEmail()', () => {
+    it('normal case', async () => {
+      // given
+      const user = new User();
+      user.id = 1;
+      user.name = '인물1';
+      user.email = 'test@naver.com';
+      jest.spyOn(userRepository, 'getOneByEmail').mockResolvedValue(user);
+
+      const email = 'test@naver.com';
+
+      // when
+      const result = await service.getUserByEmail(email);
+
+      // then
+      expect(userRepository.getOneByEmail).toHaveBeenCalledWith(email);
+      expect(userRepository.getOneByEmail).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(user);
+    });
+  });
+
+  describe('add()', () => {
+    it('normal case', async () => {
+      // given
+      const user = new User();
+      user.id = 1;
+      user.email = 'test@naver.com';
+      user.password = '12345678';
+      user.name = '김동현';
+      jest.spyOn(userRepository, 'add').mockResolvedValue(user);
+
+      const args: IAddUser = {
+        email: 'test@naver.com',
+        password: '12345678',
+        name: '김동현',
+      };
+
+      // when
+      const result = await service.addUser(args);
+
+      // then
+      expect(userRepository.add).toHaveBeenCalledWith(args);
+      expect(userRepository.add).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(user);
+    });
   });
 });
---
+++ test/repository/user.repository.mock.ts
@@ -0,0 +1,6 @@
+import { User } from '../../src/user/entity';
+
+export const MockUserRepository = () => ({
+  getOneByEmail: jest.fn().mockResolvedValue(new User()),
+  add: jest.fn().mockResolvedValue(new User()),
+});
